9d7077cafe2efa938307af5ce33c108f4061d836
Christopher Smith <Chris.Software.Developer@gmail.com>
2017-11-14 18:50:20 -0800

    - Added logic for displaying current day and time - Added label font sizes - Created Currency enum

--- Network/Controllers/ViewController.swift
+++ Network/Controllers/ViewController.swift
@@ -10,12 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //  MARK: - Properties
+    
     let urlString = "https://api.cryptonator.com/api/ticker/btc-usd"
+    
+    // MARK: - IBOutlets
     
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var currentTimeLabel: UILabel!
     
-    @IBAction func buttonPushed(_ sender: Any) {
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let currentDateTime = Date()
+        let dateFormatter = DateFormatter()
+        let todayDate = Date()
+        let myCalendar = Calendar(identifier: .gregorian)
+        let myComponents = myCalendar.component(.weekday, from: todayDate)
+        let weekDay = myComponents
+        
+        func weekDayString(fromNumber weekDay: Int) -> String {
+            
+            switch weekDay {
+                
+            case 0:
+                return "Sunday"
+            case 1:
+                return "Monday"
+            case 2:
+                return "Tuesday"
+            case 3:
+                return "Wednesday"
+            case 4:
+                return "Thursday"
+            case 5:
+                return "Friday"
+            case 6:
+                return "Saturday"
+            default:
+                return "Unknown day"
+            }
+        }
+        
+        dateFormatter.timeStyle = .short
+        self.title = "Bitcoin Value"
+        
+        let finalTime = weekDayString(fromNumber: weekDay) + " " + dateFormatter.string(from: currentDateTime)
+        
+        self.currentTimeLabel.text = finalTime
+        self.currencyLabel.font = UIFont(name: currencyLabel.font.fontName, size: 60)
+        self.valueLabel.font = UIFont(name: valueLabel.font.fontName, size: 60)
+        self.currentTimeLabel.font = UIFont(name: currentTimeLabel.font.fontName, size: 18)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         
         guard let url = URL(string: urlString) else {
             print("Error: Could not create Url")
@@ -44,7 +95,6 @@ class ViewController: UIViewController {
                                 DispatchQueue.main.async { [unowned self] in
                                     self.valueLabel.text = value
                                 }
-                                
                             }
                         }
                     }
@@ -56,5 +106,3 @@ class ViewController: UIViewController {
         task.resume()
     }
 }
-
-
---
+++ Network/Models/Currency.swift
@@ -0,0 +1,16 @@
+//
+//  Currency.swift
+//  Network
+//
+//  Created by Christopher Smith on 11/14/17.
+//  Copyright © 2017 Christopher Smith. All rights reserved.
+//
+
+import Foundation
+
+enum Currency {
+    
+    case name
+    case value
+    case time
+}
